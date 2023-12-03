3e7cbbca8b58119443b7a3321d89328994d8dc02
d2wei <d2wei@uwaterloo.ca>
2023-12-02 20:02:57 -0500

    Added methods for piece unicode output

--- project/piece.h
+++ project/piece.h
@@ -1,6 +1,7 @@
 #ifndef __PIECE_H__
 #define __PIECE_H__
 #include "colour.h"
+#include <string>
 #include <memory>
 
 enum class PieceType { King, Queen, Bishop, Rook, Knight, Pawn };
@@ -11,11 +12,22 @@ class Piece {
     bool hasMoved = false;
 public:
     Piece(Colour c, PieceType pt);
+
+    // returns a char representation of the piece
     virtual char getSymbol() const = 0;
+
+    // returns a unicode string representation of the piece
+    virtual std::string getUnicodeSymbol() const = 0;
+
     Colour getColour() const;
     PieceType getType() const;
+
+    // sets hasMoved to true, used to indicate the piece has been moved
     void firstMove();
+
+    // returns hasMoved
     bool isMoved() const;
+
     virtual ~Piece() = default;
 };
 
@@ -23,36 +35,42 @@ class King final: public Piece {
 public:
     King(Colour c);
     virtual char getSymbol() const override;
+    virtual std::string getUnicodeSymbol() const override;
 };
 
 class Queen final: public Piece {
 public:
     Queen(Colour c);
     virtual char getSymbol() const override;
+    virtual std::string getUnicodeSymbol() const override;
 };
 
 class Bishop final: public Piece {
 public:
     Bishop(Colour c);
     virtual char getSymbol() const override;
+    virtual std::string getUnicodeSymbol() const override;
 };
 
 class Rook final: public Piece {
 public:
     Rook(Colour c);
     virtual char getSymbol() const override;
+    virtual std::string getUnicodeSymbol() const override;
 };
 
 class Knight final: public Piece {
 public:
     Knight(Colour c);
     virtual char getSymbol() const override;
+    virtual std::string getUnicodeSymbol() const override;
 };
 
 class Pawn final: public Piece {
 public:
     Pawn(Colour c);
     virtual char getSymbol() const override;
+    virtual std::string getUnicodeSymbol() const override;
 };
 
 // returns a shared_ptr to a newly allocated Piece of specified Colour c
